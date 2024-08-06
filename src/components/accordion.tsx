8ed68496276827943d25bdeebdbd12fa79d38228
deepeshyadavrd <raoshsbh6@gmail.com>
2024-08-06 17:49:21 +0530

    ...

--- src/components/accordion.tsx
+++ src/components/accordion.tsx
@@ -1,4 +1,17 @@
-
+const data =[
+    {
+        title: "This is title 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quasi optio, beatae architecto quidem minus harum, vitae, recusandae officia amet voluptatibus eos qui illo tempora culpa excepturi esse ex id",
+    },
+    {
+        title: "This is title 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quasi optio, beatae architecto quidem minus harum, vitae, recusandae officia amet voluptatibus eos qui illo tempora culpa excepturi esse ex id",
+    },
+    {
+        title: "This is title 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis quasi optio, beatae architecto quidem minus harum, vitae, recusandae officia amet voluptatibus eos qui illo tempora culpa excepturi esse ex id",
+    }
+]
 const accordion = () => {
   return (
     <div>accordion</div>
