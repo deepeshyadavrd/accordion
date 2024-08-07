7e9a194d43369d28dbbdeca7867cd259204dab69
deepeshyadavrd <raoshsbh6@gmail.com>
2024-08-07 17:20:03 +0530

    final commit

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -10,6 +10,11 @@ const AccordionItem = ({title, desc, open, toggle}: propsType) => {
   return (
     <div className='accordionItem' onClick={toggle}>
       <div className="accordionItem__flex">{title} {open ? <AiOutlineMinus /> : <AiOutlinePlus />}</div>
+
+      <div className={`desc ${open ? "show" : "" }`}>
+        <span className="hidden">{desc}</span>
+      </div>
+
     </div>
   )
 }
